b6778d290b74df68f88f0ecd1767e87ff10da056
RAJA DEMO <pradhanraja780@gmail.com>
2023-12-09 00:41:04 +0530

    POST COMMIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { NetflixComponent } from './netflix/netflix.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    NetflixComponent
   ],
   imports: [
     BrowserModule,
